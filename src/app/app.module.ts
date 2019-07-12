295750423e93c2ae7112deedcf8fc0e2191db0b0
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-07-12 05:17:12 -0400

    shared components setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlertComponent } from './_components/alert/alert.component';
+import { ImportComponent } from './_components/import/import.component';
+import { SelectComponent } from './_components/select/select.component';
+import { TransferComponent } from './_components/transfer/transfer.component';
+import { RecordComponent } from './_components/record/record.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    ImportComponent,
+    SelectComponent,
+    TransferComponent,
+    RecordComponent
   ],
   imports: [
     BrowserModule
